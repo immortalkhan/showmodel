85b7c19c6230df558710035f1e8e82075b4f0054
Asad Khan <96783510+immortalkhan@users.noreply.github.com>
2024-09-07 16:51:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,11 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 
 btnCloseModal.addEventListener('click', close);
 overlay.addEventListener('click', close);
+
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('.hidden')) {
+    close();
+  }
+});
+
